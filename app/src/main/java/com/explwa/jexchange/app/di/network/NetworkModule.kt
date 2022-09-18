e6daa479e9cc702c48d8b8786c13031c54c68376
Yohan <eras.projet@gmail.com>
2022-09-18 02:31:42 +0200

    Added DI network module for retrofit

---
+++ app/src/main/java/com/explwa/jexchange/app/di/network/NetworkModule.kt
@@ -0,0 +1,27 @@
+package com.explwa.jexchange.app.di.network
+
+import com.explwa.jexchange.data.network.api.ElrondApi
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun providesElrondApi(): ElrondApi =
+        Retrofit.Builder()
+            .baseUrl("https://api.elrond.com/")
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build().create(ElrondApi::class.java)
+}
