73c0c63414317d456a2d3edc53659473859005b4
Yohan <eras.projet@gmail.com>
2022-09-18 10:38:23 +0200

    Moved directory

---
+++ app/src/main/java/com/explwa/jexchange/data/response/elrond/AssetsResponse.kt
@@ -0,0 +1,17 @@
+package com.explwa.jexchange.data.response.elrond
+
+import com.google.gson.annotations.SerializedName
+
+
+data class AssetsResponse (
+
+    @SerializedName("website") var website : String? = null,
+    @SerializedName("description") var description : String? = null,
+    @SerializedName("social") var socialResponse : SocialResponse? = SocialResponse(),
+    @SerializedName("ledgerSignature") var ledgerSignature : String? = null,
+    @SerializedName("status") var status : String? = null,
+    @SerializedName("extraTokens") var extraTokens : ArrayList<String> = arrayListOf(),
+    @SerializedName("pngUrl") var pngUrl : String? = null,
+    @SerializedName("svgUrl") var svgUrl : String? = null
+
+)
