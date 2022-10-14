dcc7b8b6a2b8f74d7f22cea0e49ae436334a9951
Yohan <eras.projet@gmail.com>
2022-10-14 02:58:23 +0200

    Added some attributes

--- app/src/main/java/com/explwa/jexchange/data/response/elrond/OperationsResponse.kt
+++ app/src/main/java/com/explwa/jexchange/data/response/elrond/OperationsResponse.kt
@@ -5,11 +5,17 @@ import com.google.gson.annotations.SerializedName
 
 data class OperationsResponse (
 
-    @SerializedName("id"       ) var id       : String? = null,
-    @SerializedName("action"   ) var action   : String? = null,
-    @SerializedName("type"     ) var type     : String? = null,
-    @SerializedName("sender"   ) var sender   : String? = null,
-    @SerializedName("receiver" ) var receiver : String? = null,
-    @SerializedName("data"     ) var data     : String? = null
+    @SerializedName("id"         ) var id         : String? = null,
+    @SerializedName("action"     ) var action     : String? = null,
+    @SerializedName("type"       ) var type       : String? = null,
+    @SerializedName("esdtType"   ) var esdtType   : String? = null,
+    @SerializedName("identifier" ) var identifier : String? = null,
+    @SerializedName("name"       ) var name       : String? = null,
+    @SerializedName("sender"     ) var sender     : String? = null,
+    @SerializedName("receiver"   ) var receiver   : String? = null,
+    @SerializedName("value"      ) var value      : String? = null,
+    @SerializedName("decimals"   ) var decimals   : Int?    = null,
+    @SerializedName("svgUrl"     ) var svgUrl     : String? = null,
+    @SerializedName("data"       ) var data       : String? = null
 
-)
+    )
