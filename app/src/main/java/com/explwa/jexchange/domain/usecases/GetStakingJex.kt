2b3f9f97a91b4325df9ce43fa73dff4bd91a91a2
Yohan <eras.projet@gmail.com>
2022-10-21 00:44:31 +0200

    Update indentation

--- app/src/main/java/com/explwa/jexchange/domain/usecases/GetStakingJex.kt
+++ app/src/main/java/com/explwa/jexchange/domain/usecases/GetStakingJex.kt
@@ -6,15 +6,13 @@ import io.reactivex.rxjava3.core.Single
 import java.math.BigInteger
 import javax.inject.Inject
 
-
 class GetStakingJex @Inject constructor(
     private val repository: StakingRepository
 ) {
 
-
-    fun getStaking(address: String) : Single<BigInteger> {
-
-        return getEnterStaking(address)
+    fun getStaking(address: String)
+    : Single<BigInteger> =
+        getEnterStaking(address)
             .flatMap { enterStaking ->
                 getExitStaking(address)
                     .flatMap { exitStaking ->
@@ -28,11 +26,9 @@ class GetStakingJex @Inject constructor(
                     }
             }
 
-    }
-
-    private fun getEnterStaking(address: String): Single<BigInteger> {
-
-        return repository.getAddressWithUsername(address)
+    private fun getEnterStaking(address: String)
+    : Single<BigInteger> =
+        repository.getAddressWithUsername(address)
             .flatMap { username ->
                 repository.getEnterStakingCount(username.address.toString())
                     .flatMap { size ->
@@ -43,11 +39,9 @@ class GetStakingJex @Inject constructor(
                     }
             }
 
-    }
-
-    private fun getExitStaking(address: String): Single<BigInteger> {
-
-        return repository.getAddressWithUsername(address)
+    private fun getExitStaking(address: String)
+    : Single<BigInteger> =
+        repository.getAddressWithUsername(address)
             .flatMap { username ->
                 repository.getExitStakingCount(username.address.toString())
                     .flatMap { size ->
@@ -58,45 +52,48 @@ class GetStakingJex @Inject constructor(
                     }
             }
 
-    }
-
-    private fun getExitStakingWithPenalty(address: String): Single<BigInteger> {
-
-        return repository.getAddressWithUsername(address)
+    private fun getExitStakingWithPenalty(address: String)
+    : Single<BigInteger> =
+        repository.getAddressWithUsername(address)
             .flatMap { username ->
                 repository.getExitStakingWithPenaltyCount(username.address.toString())
                     .flatMap { size ->
                         repository.getExitStakingWithPenalty(username.address.toString(), size)
                             .map {
-                                sumBigInteger(it, false)
+                                sumBigInteger(
+                                    it,
+                                    false
+                                )
                             }
                     }
             }
 
-    }
-
-    private fun sumBigInteger(txs: List<TransactionsResponse>, enterStaking : Boolean) : BigInteger {
-
+    private fun sumBigInteger(
+        txs: List<TransactionsResponse>,
+        enterStaking : Boolean
+    ): BigInteger {
         var int = BigInteger("0")
-
-        if(enterStaking)
-            for (tx in txs)
-                if (tx.function != null)
+        if (enterStaking)
+            for (tx1 in txs)
+                if (tx1.function != null)
                     int = int.add(
                         BigInteger(
-                            tx.action?.argumentsResponse?.transfers?.get(0)?.value.toString()
+                            tx1.action?.
+                            argumentsResponse?.
+                            transfers?.
+                            get(0)?.
+                            value.toString()
                         )
                     )
-        else
-            for (tx in txs)
-                if(tx.operations != null && tx.operations!![0].value != null)
-                    int = int.add(
-                        BigInteger(
-                            tx.operations!![0].value!!
-                        )
-                    )
+                else
+                    for (tx2 in txs)
+                        if (tx2.operations != null && tx2.operations!![0].value != null)
+                            int = int.add(
+                                BigInteger(
+                                    tx2.operations!![0].
+                                    value!!
+                                )
+                            )
         return int
-
     }
-
 }
