d5f4b52218b1f7b2d3227207d1894cefbebbc601
Yohan <eras.projet@gmail.com>
2023-01-09 06:54:00 +0100

    Removed useless usecase

--- app/src/main/java/com/explwa/jexchange/presenter/viewModels/StakingViewModel.kt
+++ app/src/main/java/com/explwa/jexchange/presenter/viewModels/StakingViewModel.kt
@@ -2,7 +2,6 @@ package com.explwa.jexchange.presenter.viewModels
 
 import androidx.lifecycle.ViewModel
 import com.explwa.jexchange.data.response.elrond.TokenResponse
-import com.explwa.jexchange.domain.usecases.GetStakingJex
 import com.explwa.jexchange.domain.usecases.GetStakingRewards
 import com.explwa.jexchange.presenter.util.MySchedulers
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,7 +11,6 @@ import javax.inject.Inject
 
 @HiltViewModel
 class StakingViewModel @Inject constructor(
-    private val stakingUseCase: GetStakingJex,
     private val rewardUseCase: GetStakingRewards,
     private val mySchedulers: MySchedulers
 ) : ViewModel() {
