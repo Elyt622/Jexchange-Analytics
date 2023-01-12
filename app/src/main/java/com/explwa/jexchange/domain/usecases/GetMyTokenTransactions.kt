f860ac087a0529895dff4b650a7a8c9b91f72deb
Yohan <eras.projet@gmail.com>
2023-01-12 06:20:19 +0100

    Added paging to get all transactions for a token, moved get all tokens function

--- app/src/main/java/com/explwa/jexchange/domain/usecases/GetMyTokenTransactions.kt
+++ app/src/main/java/com/explwa/jexchange/domain/usecases/GetMyTokenTransactions.kt
@@ -1,8 +1,8 @@
 package com.explwa.jexchange.domain.usecases
 
-import com.explwa.jexchange.domain.models.DomainTransaction
+import com.explwa.jexchange.domain.models.DomainTransactionPage
 import com.explwa.jexchange.domain.repositories.TransactionsRepository
-import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.core.Observable
 import javax.inject.Inject
 
 
@@ -10,52 +10,32 @@ class GetMyTokenTransactions @Inject constructor(
     private val repository: TransactionsRepository
 ) {
 
-    fun getMyTokenTransfers(address: String, token: String)
-    : Single<List<DomainTransaction>> =
-        repository.getAddressWithUsername(address)
-            .flatMap { username ->
-                repository.getMyTokenTransfersCount(
-                    username.address.toString(),
-                    token
-                ).flatMap { size ->
-                        repository.getMyTokenTransfers(
-                            username.address.toString(),
-                            token,
-                            13,
-                            0
-                        )
-                    }
-            }.onErrorResumeNext {
-                repository.getMyTokenTransfersCount(
-                    address,
-                    token
-                ).flatMap { size ->
-                        repository.getMyTokenTransfers(
-                            address,
-                            token,
-                            13,
-                            0
-                        )
-                    }
-            }.doOnError {
-                if (address.isNotEmpty())
-                    throw Exception("INVALID_ADDRESS")
-            }
+    private var txPageArrayList = ArrayList<DomainTransactionPage>()
+    private var from : Int = 0
 
-    fun getTransactionWithHash(txHash: String)
-    : Single<DomainTransaction> =
-        repository.getTransactionWithHash(txHash)
-
-    fun getAllTokensOnJexchange()
-    : Single<MutableList<String>> =
-        repository.getTokensCountOnJexchange()
-            .flatMap {
-                repository.getAllTokensOnJexchange(it)
-                    .toObservable()
-                    .flatMapIterable { it }
-                    .map { token ->
-                        token.identifier.toString()
-                    }.toList()
+    fun invoke(
+        address: String,
+        token: String,
+        refresh: Boolean = false
+    ): Observable<List<DomainTransactionPage>> =
+        Observable.defer {
+            if (refresh) {
+                txPageArrayList = ArrayList()
+                from = 0
+            } else {
+                from += 13
             }
+            repository.getTxPage(
+                address,
+                token,
+                13,
+                from
+            )
+                .map {
+                    txPageArrayList.apply {
+                        add(it)
+                    }
+                }.toObservable()
+        }
 
 }
