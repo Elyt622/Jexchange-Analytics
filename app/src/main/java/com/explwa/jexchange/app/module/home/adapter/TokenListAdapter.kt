b0d58d96f4e03981d6738a7c53d55980927f6765
Yohan <eras.projet@gmail.com>
2023-01-12 06:30:31 +0100

    Converted token response into UiTokenItem

--- app/src/main/java/com/explwa/jexchange/app/module/home/adapter/TokenListAdapter.kt
+++ app/src/main/java/com/explwa/jexchange/app/module/home/adapter/TokenListAdapter.kt
@@ -7,27 +7,27 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.explwa.jexchange.app.module.home.utils.Utils
-import com.explwa.jexchange.data.response.elrond.TokenResponse
 import com.explwa.jexchange.databinding.ViewHolderTokensBinding
+import com.explwa.jexchange.presenter.model.UITokenItem
 
 // Recycler View Adapter into List Adapter
 // https://medium.com/codex/stop-using-recyclerview-adapter-27c77666512b
 
 class TokenListAdapter(
-    data: List<TokenResponse>
-) : ListAdapter<TokenResponse, TokenListAdapter.ViewHolder>(callback) {
+    data: List<UITokenItem>
+) : ListAdapter<UITokenItem, TokenListAdapter.ViewHolder>(callback) {
 
     companion object {
-        val callback = object : DiffUtil.ItemCallback<TokenResponse>() {
-            override fun areItemsTheSame(oldItem: TokenResponse, newItem: TokenResponse) =
-                oldItem.identifier == newItem.identifier
+        val callback = object : DiffUtil.ItemCallback<UITokenItem>() {
+            override fun areItemsTheSame(oldItem: UITokenItem, newItem: UITokenItem) =
+                oldItem == newItem
 
-            override fun areContentsTheSame(oldItem: TokenResponse, newItem: TokenResponse) =
+            override fun areContentsTheSame(oldItem: UITokenItem, newItem: UITokenItem) =
                 oldItem == newItem
         }
     }
 
-    var list: List<TokenResponse>
+    var list: List<UITokenItem>
         get() = currentList
         set(value) {
             submitList(value)
@@ -48,7 +48,7 @@ class TokenListAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(getItem(position))
+        holder.bind(getItem(position) as UITokenItem.Cell)
     }
 
     override fun getItemCount(): Int {
@@ -57,13 +57,13 @@ class TokenListAdapter(
 
     inner class ViewHolder(private val binding: ViewHolderTokensBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind (tokenResponse: TokenResponse) {
+        fun bind (token: UITokenItem.Cell) {
             with(binding) {
-                Glide.with(root.context).load(tokenResponse.assets?.pngUrl).into(imageViewToken)
-                Glide.with(root.context).load(tokenResponse.assets?.pngUrl).into(imageViewLogoBackground)
-                textviewTokenId.text = tokenResponse.identifier
-                textviewPriceToken.text = Utils.toFormatString(tokenResponse.price.toString())
-                textviewTokenName.text = tokenResponse.name
+                Glide.with(root.context).load(token.pngUrl).into(imageViewToken)
+                Glide.with(root.context).load(token.pngUrl).into(imageViewLogoBackground)
+                textviewTokenId.text = token.identifier
+                textviewPriceToken.text = Utils.toFormatString(token.price.toString())
+                textviewTokenName.text = token.name
             }
         }
 
