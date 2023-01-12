668c754c6da5c5f7ece632fe02570eafb5007a6f
Yohan <eras.projet@gmail.com>
2023-01-12 06:31:49 +0100

    Refactored Staking to match with Clean Architecture Concept (I hope)

--- app/src/main/java/com/explwa/jexchange/domain/usecases/GetStakingRewards.kt
+++ app/src/main/java/com/explwa/jexchange/domain/usecases/GetStakingRewards.kt
@@ -1,6 +1,6 @@
 package com.explwa.jexchange.domain.usecases
 
-import com.explwa.jexchange.data.response.elrond.TokenResponse
+import com.explwa.jexchange.domain.models.DomainToken
 import com.explwa.jexchange.domain.repositories.StakingRepository
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
@@ -8,7 +8,7 @@ import javax.inject.Inject
 class GetStakingRewards @Inject constructor(
     private val repository: StakingRepository
 ){
-    fun getStakingRewards() : Single<List<TokenResponse>> {
+    fun getStakingRewards() : Single<List<DomainToken>> {
         return repository.getStakingRewards()
             .toObservable()
             .flatMapIterable { it }
