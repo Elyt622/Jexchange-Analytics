64b39e4fd372752cd56bc974a9b569189c1c3544
Yohan <eras.projet@gmail.com>
2023-02-20 02:18:44 +0100

    Add get view state function and format view model

--- app/src/main/java/com/explwa/jexchangeanalytics/app/module/marketplace/fragment/MarketplaceFragment.kt
+++ app/src/main/java/com/explwa/jexchangeanalytics/app/module/marketplace/fragment/MarketplaceFragment.kt
@@ -1,19 +1,21 @@
 package com.explwa.jexchangeanalytics.app.module.marketplace.fragment
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isGone
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.explwa.jexchangeanalytics.app.module.marketplace.adapter.TokenListAdapter
-import com.explwa.jexchangeanalytics.presenter.viewModels.MarketplaceViewModel
+import com.explwa.jexchangeanalytics.app.utils.BaseFragment
 import com.explwa.jexchangeanalytics.databinding.FragmentMarketplaceBinding
+import com.explwa.jexchangeanalytics.presenter.viewModels.MarketplaceViewModel
+import com.jakewharton.rxbinding4.swiperefreshlayout.refreshes
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.kotlin.subscribeBy
 
 
@@ -24,6 +26,8 @@ class MarketplaceFragment : BaseFragment() {
 
     private lateinit var binding: FragmentMarketplaceBinding
 
+    var adapter = TokenListAdapter(listOf())
+
     companion object {
         fun newInstance() = MarketplaceFragment()
     }
@@ -33,60 +37,56 @@ class MarketplaceFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMarketplaceBinding.inflate(layoutInflater)
+        configRecyclerView()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        updateUi(MarketplaceViewModel.HomeViewModelStateLoading())
-        loadTokens()
-
-        configRecyclerView()
-
-        with(binding) {
-            textviewLinkExchange.setOnClickListener {
-                launchJexchangeOnBrowser()
-            }
-        }
-    }
-
-    private fun updateUi(state : MarketplaceViewModel.ViewState) {
-        with(binding) {
-            when (state) {
-                is MarketplaceViewModel.HomeViewModelStateLoading -> {
-                    progressCircular.isGone = state.progressBarVisibility
+        viewModel.getViewState(
+            viewCreated = viewCreatedSubject,
+            displayProgress = adapter.displayProgressSubject,
+            refresh = binding.swipe.refreshes()
+        ).subscribeBy(
+            onNext = { state ->
+                when (state) {
+                    is MarketplaceViewModel.ShowLoading -> {
+                        with(binding) {
+                            progressCircular.isGone = state.progressBarGone
+                        }
+                    }
+                    is MarketplaceViewModel.ShowTokens -> {
+                        with(binding) {
+                            swipe.isRefreshing = false
+                            progressCircular.isGone = state.progressBarGone
+                            Log.d("DEBUG", state.tokens.size.toString())
+                            adapter.submitList(state.tokens)
+                        }
+                    }
+                    is MarketplaceViewModel.ShowError -> {
+                        binding.swipe.isRefreshing = false
+                        Toast.makeText(
+                            requireContext(),
+                            state.errorMessage,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    else -> { }
                 }
-                is MarketplaceViewModel.HomeViewModelStateSuccess -> {
-                    progressCircular.isGone = state.progressBarVisibility
-                    recyclerViewTokens.adapter = TokenListAdapter(state.tokens)
-                }
+                MarketplaceViewModel.ShowTokens(state.tokens)
+            },
+            onError = {
+                MarketplaceViewModel.ShowError(it.message.toString())
+            }
+        ).addTo(disposable)
+    }
+
+
+        private fun configRecyclerView() {
+            with(binding) {
+                recyclerViewTokens.layoutManager = LinearLayoutManager(requireContext())
+                recyclerViewTokens.adapter = adapter
             }
         }
-    }
-
-    private fun configRecyclerView() {
-        with(binding) {
-            recyclerViewTokens.layoutManager = LinearLayoutManager(requireContext())
-            recyclerViewTokens.adapter = TokenListAdapter(listOf())
-        }
-    }
-
-    private fun loadTokens() {
-        viewModel.getAllTokens()
-            .subscribeBy {
-                updateUi(MarketplaceViewModel.HomeViewModelStateSuccess(it))
-            }
-    }
-
-    private fun launchJexchangeOnBrowser() {
-        with(binding) {
-            startActivity(
-                Intent(
-                    Intent.ACTION_VIEW,
-                    Uri.parse("https://" + textviewLinkExchange.text.toString())
-                )
-            )
-        }
-    }
 }
--- app/src/main/java/com/explwa/jexchangeanalytics/presenter/viewModels/MarketplaceViewModel.kt
+++ app/src/main/java/com/explwa/jexchangeanalytics/presenter/viewModels/MarketplaceViewModel.kt
@@ -6,7 +6,8 @@ import com.explwa.jexchangeanalytics.presenter.model.UITokenItem
 import com.explwa.jexchangeanalytics.presenter.model.mapping.toUIItem
 import com.explwa.jexchangeanalytics.presenter.util.MySchedulers
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.core.Observable
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,21 +16,40 @@ class MarketplaceViewModel @Inject constructor(
     private val mySchedulers: MySchedulers
 ) : ViewModel() {
 
-    sealed class ViewState (
-        val tokens : List<UITokenItem>,
-        val progressBarVisibility : Boolean
-    )
+    sealed class ViewState(
+        val tokens: List<UITokenItem>,
+        val progressBarGone: Boolean,
+        val errorMessage: String
+        )
 
-    class HomeViewModelStateLoading : ViewState(listOf(), false)
-    class HomeViewModelStateSuccess(tokens: List<UITokenItem>) : ViewState(tokens, true)
+    class ShowLoading : ViewState(listOf(), false, "")
+    class ShowTokens(tokens: List<UITokenItem>) : ViewState(tokens, true, "")
+    class ShowError(errorMessage: String) : ViewState(listOf(), true, errorMessage)
+    object ShowNone : ViewState(listOf(), true, "")
 
-    fun getAllTokens(): Single<List<UITokenItem>> {
-        return usecase.getAllTokens()
+    fun getViewState(
+        viewCreated: Observable<Unit>,
+        displayProgress: Observable<Unit>,
+        refresh: Observable<Unit>
+    ): Observable<ViewState> =
+        Observable.merge(
+            viewCreated.map { false },
+            displayProgress.map { false },
+            refresh.map { true }
+        )
+            .toFlowable(BackpressureStrategy.DROP) //load/refresh should be finished before load/refresh again
             .toObservable()
-            .flatMapIterable { it }
-            .map { it.toUIItem() }
-            .toList()
-            .subscribeOn(mySchedulers.io)
-            .observeOn(mySchedulers.main)
-    }
+            .concatMap {
+                usecase.invoke(
+                    it
+                ).subscribeOn(mySchedulers.io)
+            }.observeOn(mySchedulers.main)
+            .map { tokenPageList ->
+                val tokenList = tokenPageList.toUIItem()
+                if (tokenList.isEmpty()) ShowNone
+                else ShowTokens(tokenList.distinct())
+            }.doOnError {
+                ShowError(it.message.toString())
+            }
+
 }
