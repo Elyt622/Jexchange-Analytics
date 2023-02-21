7b9705461e8a2af8b158c64046ee62f574fa0ab8
Yohan <eras.projet@gmail.com>
2023-02-21 05:02:26 +0100

    Change bottom navigation for fragments position

--- app/src/main/java/com/explwa/jexchangeanalytics/app/module/main/adapter/ViewpagerAdapter.kt
+++ app/src/main/java/com/explwa/jexchangeanalytics/app/module/main/adapter/ViewpagerAdapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.explwa.jexchangeanalytics.app.module.marketplace.fragment.MarketplaceFragment
-import com.explwa.jexchangeanalytics.app.module.mytxs.fragment.MyTxsFragment
+import com.explwa.jexchangeanalytics.app.module.portfolio.fragment.PortfolioFragment
 import com.explwa.jexchangeanalytics.app.module.staking.fragment.StakingFragment
 
 
@@ -17,8 +17,8 @@ class ViewpagerAdapter(fa: FragmentActivity)
 
     override fun createFragment(position: Int): Fragment {
         when (position) {
-            0 -> return MarketplaceFragment.newInstance()
-            1 -> return MyTxsFragment.newInstance()
+            0 -> return PortfolioFragment.newInstance()
+            1 -> return MarketplaceFragment.newInstance()
             2 -> return StakingFragment.newInstance()
         }
         return MarketplaceFragment.newInstance()
