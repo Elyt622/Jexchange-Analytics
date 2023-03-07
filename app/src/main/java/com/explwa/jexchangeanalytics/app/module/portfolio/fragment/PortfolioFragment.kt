32e914742a7ae56ccadc9f938cdb0938cfd1c2bd
Yohan <eras.projet@gmail.com>
2023-03-07 09:47:13 +0100

    Add view state & function to get the token list

--- app/src/main/java/com/explwa/jexchangeanalytics/app/module/portfolio/fragment/PortfolioFragment.kt
+++ app/src/main/java/com/explwa/jexchangeanalytics/app/module/portfolio/fragment/PortfolioFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.explwa.jexchangeanalytics.app.module.main.activity.MainActivity
+import com.explwa.jexchangeanalytics.app.module.portfolio.adapter.TokenListAdapter
 import com.explwa.jexchangeanalytics.app.module.portfolio.utils.Utils
 import com.explwa.jexchangeanalytics.app.utils.BaseFragment
 import com.explwa.jexchangeanalytics.databinding.FragmentPortfolioBinding
@@ -23,30 +25,60 @@ class PortfolioFragment : BaseFragment() {
 
     val viewModel: PortfolioViewModel by viewModels()
 
+    var adapter = TokenListAdapter(listOf())
+
+    lateinit var currentAddress : String
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPortfolioBinding.inflate(layoutInflater)
+        configRecyclerView()
+        currentAddress = (activity as MainActivity).currentAccount
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.getTotalValue((activity as MainActivity).currentAccount)
+        viewModel.getViewState(
+            currentAddress,
+            viewCreatedSubject,
+            adapter.displayProgressSubject
+        ).subscribeBy {
+            adapter.submitList(it.tokens)
+        }.addTo(disposable)
+
+        viewModel.getTotalValue(currentAddress)
             .subscribeBy {
-                binding.textviewPortfolioValue.text = it.setScale(
-                    2,
-                    RoundingMode.UP
-                ).toPlainString()
+                binding.textviewPortfolioValue.text =
+                    it.setScale(
+                        2,
+                        RoundingMode.UP
+                    ).toPlainString()
             }.addTo(disposable)
 
-       viewModel.getJexBalance((activity as MainActivity).currentAccount)
+       viewModel.getJexBalance(currentAddress)
            .subscribeBy {
                 binding.textviewJexBalance.text =
-                    Utils.fromBigIntegerToBigDecimal(it[0].balance, it[0].decimals).toPlainString()
+                    Utils.fromBigIntegerToBigDecimal(
+                        it[0].balance,
+                        it[0].decimals
+                    ).toPlainString()
             }.addTo(disposable)
+    }
+
+    private fun configRecyclerView() {
+        with(binding) {
+            rvPortfolio.layoutManager =
+                LinearLayoutManager(
+                    requireContext(),
+                    LinearLayoutManager.HORIZONTAL,
+                    false
+                )
+            rvPortfolio.adapter = adapter
+        }
     }
 
     companion object {
--- app/src/main/java/com/explwa/jexchangeanalytics/presenter/viewModels/PortfolioViewModel.kt
+++ app/src/main/java/com/explwa/jexchangeanalytics/presenter/viewModels/PortfolioViewModel.kt
@@ -2,20 +2,35 @@ package com.explwa.jexchangeanalytics.presenter.viewModels
 
 import androidx.lifecycle.ViewModel
 import com.explwa.jexchangeanalytics.domain.models.DomainToken
+import com.explwa.jexchangeanalytics.domain.usecases.GetAllTokens
 import com.explwa.jexchangeanalytics.domain.usecases.GetBalance
 import com.explwa.jexchangeanalytics.domain.usecases.GetValueFromAccount
+import com.explwa.jexchangeanalytics.presenter.model.UITokenItem
+import com.explwa.jexchangeanalytics.presenter.model.mapping.toUIItem
 import com.explwa.jexchangeanalytics.presenter.util.MySchedulers
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import java.math.BigDecimal
 import javax.inject.Inject
 
 @HiltViewModel
 class PortfolioViewModel @Inject constructor(
+    private val getAllTokens: GetAllTokens,
     private val getBalance: GetBalance,
     private val getValueFromAccount: GetValueFromAccount,
     private val mySchedulers: MySchedulers
 ) : ViewModel() {
+
+    sealed class ViewState(
+        val tokens: List<UITokenItem>,
+        val errorMessage: String
+    )
+
+    class ShowTokens(tokens: List<UITokenItem>) : ViewState(tokens, "")
+    class ShowError(errorMessage: String) : ViewState(listOf(), errorMessage)
+    object ShowNone : ViewState(listOf(), "")
 
     fun getJexBalance(address: String)
     : Single<List<DomainToken>> =
@@ -28,6 +43,31 @@ class PortfolioViewModel @Inject constructor(
         getValueFromAccount.getTotalValue(address)
             .subscribeOn(mySchedulers.io)
             .observeOn(mySchedulers.main)
+
+    fun getViewState(
+        address: String,
+        viewCreated: Observable<Unit>,
+        displayProgress: Observable<Unit>,
+    ): Observable<ViewState> =
+        Observable.merge(
+            viewCreated.map { false },
+            displayProgress.map { false },
+        )
+            .toFlowable(BackpressureStrategy.DROP) //load/refresh should be finished before load/refresh again
+            .toObservable()
+            .concatMap {
+                getAllTokens.invoke(
+                    address,
+                    it
+                ).subscribeOn(mySchedulers.io)
+            }.observeOn(mySchedulers.main)
+            .map { tokenPageList ->
+                val tokenList = tokenPageList.toUIItem()
+                if (tokenList.isEmpty()) ShowNone
+                else ShowTokens(tokenList.distinct())
+            }.doOnError {
+                ShowError(it.message.toString())
+            }
 
     companion object {
         const val JEX_ID = "JEX-9040ca"
